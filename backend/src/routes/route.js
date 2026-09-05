93de0713b83284b58ef9fbf55f9b4f6ae32775a8
Dirag Kasula <190046038+dir599@users.noreply.github.com>
2026-09-05 20:07:34 +0545

    feat: add blog model

--- backend/src/routes/route.js
+++ backend/src/routes/route.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import authRouter from "./auth.routes.js";
 import userRouter from "./user.routes.js";
+import blogRouter from "./blog.routes.js"
 
 const router = new Router();
 router.use("/auth", authRouter);
 router.use("/user", userRouter);
+router.use("/blog", blogRouter)
 
 export default router;
