884c737740ed0451d51ac5fba76e197d0e32cc2a
Dirag Kasula <190046038+dir599@users.noreply.github.com>
2026-09-06 21:50:06 +0545

    feat: add delete and get blog with rbac

--- backend/src/controllers/blog.controller.js
+++ backend/src/controllers/blog.controller.js
@@ -1,7 +1,11 @@
 import {
   createBlogService,
+  deleteBlogService,
+  getAllBlogServices,
+  getBlogByIdService,
   updateBlogServices,
 } from "../services/blog.service.js";
+import ApiError from "../utils/apiError.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
 const createBlog = asyncHandler(async (req, res) => {
@@ -54,4 +58,42 @@ const updateBlog = asyncHandler(async (req, res) => {
   });
 });
 
-export { createBlog, updateBlog };
+const getBlog = asyncHandler(async (req, res) => {
+  const blog = await getAllBlogServices();
+  return res.status(200).json({
+    success: true,
+    message: "All blogs",
+    data: blog,
+  });
+});
+
+const getBlogById = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  if (isNaN(id)) {
+    throw new ApiError(400, "Id must be number.");
+  }
+  const blog = await getBlogByIdService({ id });
+  return res.status(200).json({
+    success: true,
+    message: "Blog get by Id",
+    data: blog,
+  });
+});
+
+const deleteBlog = asyncHandler(async (req, res) => {
+  const {id} = req.params;
+  const userId = req.user.id
+  const role = req.user.role
+  if (isNaN(id)) {
+    throw new ApiError(400, "Id is not valid");
+  }
+ 
+  const blog = await deleteBlogService({ id: Number(id), userId, role });
+  return res.status(200).json({
+    success: true,
+    message: "Blog delete",
+    data: blog,
+  });
+});
+
+export { createBlog, updateBlog, getBlog, getBlogById, deleteBlog};
