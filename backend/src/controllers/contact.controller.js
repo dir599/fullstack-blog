fd3a5e6ed00c4705115cbea14ecbbd4a2812a37e
Dirag Kasula <190046038+dir599@users.noreply.github.com>
2026-09-17 21:12:22 +0545

    feat:update contact page

--- backend/src/controllers/contact.controller.js
+++ backend/src/controllers/contact.controller.js
@@ -1,9 +1,10 @@
 import {
   createContactService,
+  deleteContactService,
   getContactService,
 } from "../services/contact.service.js";
 import asyncHandler from "../utils/asyncHandler.js";
-
+import apiError from "../utils/apiError.js";
 const createContact = asyncHandler(async (req, res) => {
   const { name, email, subject, message } = req.body;
   const contact = await createContactService({ name, email, subject, message });
@@ -23,4 +24,27 @@ const getContact = asyncHandler(async (req, res) => {
   });
 });
 
-export { createContact, getContact };
+const getContactById = asyncHandler(async (req, res) => {
+  const { id } = req.user.params;
+  if (!id) {
+    throw new apiError(400, "id not found");
+  }
+  const contact = await getContactById({ id });
+  return res.status(200).json({
+    success: true,
+    message: "id got",
+    data: contact,
+  });
+});
+
+const deleteContact = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  const contact = await deleteContactService({ id });
+  return res.status(200).json({
+    success: true,
+    message: "Contact deleted successfully",
+    data: contact,
+  });
+});
+
+export { createContact, getContact, getContactById, deleteContact };
